9467850d39da9f139d1c069319b128ba637932ad
Elijah Jackson <elijahdj88@gmail.com>
2021-11-13 20:32:29 -0500

    finished process

--- process.py
+++ process.py
@@ -1,12 +1,12 @@
 log_file = open("um-server-01.txt")
 
 
-def sales_reports(log_file):  
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def sales_reports(log_file):  # creating a function that takes in the file from log_file
+    for line in log_file:     # looping through each line in the file
+        line = line.rstrip()  # removing the ending white space
+        day = line[0:3]       # selecting the 0 index day out of the file
+        if day == "Mon":      # if the day is Tue
+            print(line)       # print out that entire row
 
 
-sales_reports(log_file)
+sales_reports(log_file)       # invokes the function made above
